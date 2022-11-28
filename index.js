93717a826f2b12f21407dfba159997747f334b58
Kellymugambi <kellymgambi@.com>
2022-11-28 20:09:55 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let multiply = ('num1', 'num2')
+const num1 = 31;
+const num2 = 2;
+multiply = (31 * 2);
+// Write your code here/calendar
+const random = 3
 
-// Write your code here
+
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+
+const max = 20
+math.max(3,4,5,7,8,20)
